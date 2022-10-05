d17477c8a67e0a32bcba856d4ac969fdbc1f866a
ruraliz <ruraliz12@gmail.com>
2022-10-04 19:11:23 -0700

    add: Game,Board,Square component

---
+++ src/Square.js
@@ -0,0 +1,14 @@
+class Square extends React.Component {
+    render() {
+      return (
+        <button
+          className="square"
+          onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
+        </button>
+      );
+    }
+  }
+
+  export default Square;
